dbb76e2acb76daf3730e2741abf303b91f89d052
Jannatul Ferdous <jannatulferdous8558@gmail.com>
2026-03-09 00:22:51 +0600

    ReConfiguration for Vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 echo "Building the project..."
-python3.12 -m pip install -r requirements.txt
+
+# Add the --break-system-packages flag to bypass the PEP 668 protection
+python3.12 -m pip install -r requirements.txt --break-system-packages
 
 echo "Collecting static files..."
+# Use the same python version for manage.py
 python3.12 manage.py collectstatic --noinput --clear
